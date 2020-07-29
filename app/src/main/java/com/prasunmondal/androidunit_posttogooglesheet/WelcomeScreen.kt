3b43ebd32e3dc2cb67050fbd8fea60e7838cb585
Prasun <pmandal@akamai.com>
2020-07-29 23:36:11 +0530

    tech4Bytes template ready

---
+++ app/src/main/java/com/prasunmondal/androidunit_posttogooglesheet/WelcomeScreen.kt
@@ -0,0 +1,50 @@
+package com.prasunmondal.androidunit_posttogooglesheet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_welcome_screen.*
+
+class WelcomeScreen : AppCompatActivity() {
+    private val mHideHandler = Handler()
+
+    private val mShowPart2Runnable = Runnable {
+        supportActionBar?.show()
+        fullscreen_content_controls.visibility = View.VISIBLE
+    }
+    private var mVisible: Boolean = false
+    private val mHideRunnable = Runnable { hide() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_welcome_screen)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mVisible = true
+        delayedHide(100)
+
+        Handler().postDelayed({ // This method will be executed once the timer is over
+            val i = Intent(this@WelcomeScreen, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 1500)
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        fullscreen_content_controls.visibility = View.GONE
+        mVisible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable)
+    }
+
+    private fun delayedHide(delayMillis: Int) {
+        mHideHandler.removeCallbacks(mHideRunnable)
+        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    }
+}
