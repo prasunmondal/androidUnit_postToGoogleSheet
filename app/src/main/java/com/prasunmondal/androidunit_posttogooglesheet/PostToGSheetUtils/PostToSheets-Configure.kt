4697aeb0531c8cf89eaac35d1877e14a61ca4973
Prasun <pmandal@akamai.com>
2020-07-31 23:25:52 +0530

    use library

--- app/src/main/java/com/prasunmondal/androidunit_posttogooglesheet/PostToGSheetUtils/PostToSheets-Configure.kt
+++ app/src/main/java/com/prasunmondal/androidunit_posttogooglesheet/PostToGSheetUtils/PostToSheets-Configure.kt
@@ -1,8 +1,10 @@
 package com.prasunmondal.androidunit_posttogooglesheet.PostToGSheetUtils
 
+import com.prasunmondal.lib.posttogsheets.PostToGSheet
+
 class PostToSheets {
-    var po: PostToSheetDONOTUSE =
-        PostToSheetDONOTUSE(
+    var po: PostToGSheet =
+        PostToGSheet(
             "https://script.google.com/macros/s/AKfycbyoYcCSDEbXuDuGf0AhQjEi61ECAkl8JUv4ffNofz1yBIKfcT4/exec",
             "https://docs.google.com/spreadsheets/d/1gZA5tqllOArlLJb2nLcmLqfNR-cdgFzNqTl9ZKyzcOI/edit#gid=0",
             "test",
@@ -11,8 +13,8 @@ class PostToSheets {
             true, null
         )
 
-    var qo: PostToSheetDONOTUSE =
-        PostToSheetDONOTUSE(
+    var qo: PostToGSheet =
+        PostToGSheet(
             "https://script.google.com/macros/s/AKfycbyoYcCSDEbXuDuGf0AhQjEi61ECAkl8JUv4ffNofz1yBIKfcT4/exec",
             "https://docs.google.com/spreadsheets/d/1T0VRVqzwcaUCrB4_17ExJfH9cVYvwljlPj-8w6USL_8/edit#gid=0",
             "test",
